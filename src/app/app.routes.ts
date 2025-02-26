6de670152d5621dd7b5162b6886449beb93573b2
wagarcia27 <wagarcia@uninorte.edu.co>
2025-02-26 02:17:48 -0500

    Primera versión del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'sobre-nosotros', component: SobreNosotrosComponent },
+  { path: 'contacto', component: ContactoComponent }
+];
---
+++ src/app/sobre-nosotros/sobre-nosotros.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-nosotros',
+  imports: [],
+  templateUrl: './sobre-nosotros.component.html',
+  styleUrl: './sobre-nosotros.component.css'
+})
+export class SobreNosotrosComponent {
+
+}

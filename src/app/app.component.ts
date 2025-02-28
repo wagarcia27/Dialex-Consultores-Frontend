f965e7ffba5b570839c4ef0a2e3bd4218923de1a
wagarcia27 <wagarcia@uninorte.edu.co>
2025-02-28 01:21:30 -0500

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
-import { NavigationEnd, RouterOutlet } from '@angular/router';
+import { Component, ChangeDetectorRef, HostListener, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { NavigationEnd, RouterOutlet, Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 @Component({
@@ -12,44 +11,41 @@ import { filter } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent{
   title = 'dialexconsultores';
   isMenuOpen = false;
 
   constructor(private router: Router, private cdRef: ChangeDetectorRef) {
-    console.log('Constructor ejecutado');
-
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
-    ).subscribe(event => {
-      console.log('Evento de navegación detectado:', event);
-      setTimeout(() => {
-        this.isMenuOpen = false;
-        this.cdRef.detectChanges();
-        console.log('isMenuOpen actualizado a:', this.isMenuOpen);
-      }, 10); // 🔥 Le da tiempo al DOM para actualizarse antes de cerrar el menú
+    ).subscribe(() => {
+      this.cerrarMenu();
     });
-    }
-    
-
-  ngOnInit(): void {
-    console.log('ngOnInit ejecutado, estado inicial de isMenuOpen:', this.isMenuOpen);
   }
 
+  
+    
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
-    this.cdRef.detectChanges(); // 🔥 Forzar actualización del DOM
-    console.log('toggleMenu ejecutado, nuevo estado de isMenuOpen:', this.isMenuOpen);
+    this.cdRef.detectChanges();
   }
 
+
+  abrirMenu() {
+    this.isMenuOpen = true;
+    this.cdRef.detectChanges();
+  }
+  
   cerrarMenu() {
     this.isMenuOpen = false;
-    console.log("Menú cerrado");
-    document.querySelector('.custom-dropdown')?.classList.add('menu-hide');
-  
-    // Remueve la clase después de un breve tiempo para evitar que interfiera con hover
-    setTimeout(() => {
-      document.querySelector('.custom-dropdown')?.classList.remove('menu-hide');
-    }, 300);
+    this.cdRef.detectChanges();
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    const dropdown = document.querySelector('.custom-dropdown');
+    if (dropdown && !dropdown.contains(event.target as Node)) {
+      this.cerrarMenu();
+    }
   }
 }

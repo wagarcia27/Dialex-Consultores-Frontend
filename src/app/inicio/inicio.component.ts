60ec983abc0c08c8eef3a5bac456559af2803cf0
wagarcia27 <wagarcia@uninorte.edu.co>
2025-03-17 06:24:28 -0500

    subiendo front

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ImageModule } from 'primeng/image';
+import { NavigationEnd, RouterOutlet, Router, RouterModule, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [ImageModule, RouterModule, RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
